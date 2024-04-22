97feb7d063670baf32d0b38f9fbb3ba25b8550c9
Kavin <kavin.ned@gmail.com>
2024-04-22 09:23:16 +0800

    feat: pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,29 @@ function App() {
 	const [pokemon, setPokemon] = useState([]);
 	const [pokemonData, setData] = useState([]);
 	const [loading, setLoading] = useState(false);
+	const [URL, setURL] = useState(
+		"https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+	);
+	const [prevURL, setPrev] = useState("");
+	const [nextURL, setNext] = useState("");
+	const [count, setCount] = useState(1);
 
 	useEffect(() => {
 		fetchPokemon();
 		async function fetchPokemon() {
-			const URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 			const res = await fetch(URL);
 			const data = await res.json();
 			const result = data.results;
+			setNext(data.next);
+			setPrev(data.previous);
+			console.log(data);
 			setLoading(true);
 			setTimeout(() => {
 				setPokemon(result);
 				setLoading(false);
-			}, 5000);
+			}, 3000);
 		}
-	}, []);
+	}, [URL]);
 	useEffect(() => {
 		async function fetchAllPokemonData() {
 			for (let i = 0; i < pokemon.length; i++) {
@@ -31,6 +39,20 @@ function App() {
 		}
 		fetchAllPokemonData();
 	}, [pokemon]);
+
+	const nextPage = () => {
+		setURL(nextURL);
+		setPokemon([]);
+		setData([]);
+		setCount(count + 1);
+	};
+
+	const prevPage = () => {
+		setURL(prevURL);
+		setPokemon([]);
+		setData([]);
+		setCount(count - 1);
+	};
 
 	return (
 		<div className="container">
@@ -51,6 +73,17 @@ function App() {
 					</div>
 				))}
 			</div>
+			{pokemonData.length > 0 && (
+				<div className="pagination">
+					<button className="prev" onClick={prevPage}>
+						Previous Page
+					</button>
+					<h4 className="count">PAGE {count}</h4>
+					<button className="next" onClick={nextPage}>
+						Next Page
+					</button>
+				</div>
+			)}
 		</div>
 	);
 }
