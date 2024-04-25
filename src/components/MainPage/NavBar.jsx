d92491a73994c536138e96f1acfc0e78b92f906a
Kavin <kavin.ned@gmail.com>
2024-04-26 00:04:36 +0800

    feat: fetch type instead of manually listing

--- src/components/MainPage/NavBar.jsx
+++ src/components/MainPage/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavBar({
@@ -11,6 +11,17 @@ export default function NavBar({
 	const [input, setInput] = useState("");
 	const [selected, setSelected] = useState("select an option");
 	const [disableSearch, setDisableSearch] = useState(true);
+	const [types, setTypes] = useState([]);
+
+	useEffect(() => {
+		async function fetchTypes() {
+			const res = await fetch("https://pokeapi.co/api/v2/type");
+			const data = await res.json();
+			const types = data.results;
+			setTypes(types.slice(0, -2));
+		}
+		fetchTypes();
+	}, []);
 
 	const handleSearch = (event) => {
 		const value = event.target.value;
@@ -87,23 +98,11 @@ export default function NavBar({
 					<option disabled value={null}>
 						select an option
 					</option>
-					<option value="rock">Rock</option>
-					<option value="grass">Grass</option>
-					<option value="normal">Normal</option>
-					<option value="fire">Fire</option>
-					<option value="water">Water</option>
-					<option value="electric">Electric</option>
-					<option value="ice">Ice</option>
-					<option value="fighting">Fighting</option>
-					<option value="poison">Poison</option>
-					<option value="flying">Flying</option>
-					<option value="psychic">Psychic</option>
-					<option value="bug">Bug</option>
-					<option value="ghost">Ghost</option>
-					<option value="dragon">Dragon</option>
-					<option value="dark">Dark</option>
-					<option value="steel">Steel</option>
-					<option value="fairy">Fairy</option>
+					{types.map((type) => (
+						<option key={type.name} value={type.name}>
+							{type.name}
+						</option>
+					))}
 				</select>
 			</div>
 			<Link to="/favorites">
