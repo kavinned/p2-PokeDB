8cb25cc2e08fa3966d9968e87fcf7008b02277e8
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-04-26 10:47:22 +0800

    chore: remove unused isloading

--- src/components/MainPage/PokeContainer.jsx
+++ src/components/MainPage/PokeContainer.jsx
@@ -1,17 +1,13 @@
 import Card from "./Card";
-import Loader from "./Loader";
 
-export default function PokeContainer({ pokemonData, isLoading }) {
+export default function PokeContainer({ pokemonData }) {
 	return (
-		isLoading && <Loader />,
-		!isLoading && (
-			<div className="pokecontainer">
-				{pokemonData.map((pokemon) => (
-					<div className="card" key={pokemon.id}>
-						<Card pokemon={pokemon} />
-					</div>
-				))}
-			</div>
-		)
+		<div className="pokecontainer">
+			{pokemonData.map((pokemon) => (
+				<div className="card" key={pokemon.id}>
+					<Card pokemon={pokemon} />
+				</div>
+			))}
+		</div>
 	);
 }
